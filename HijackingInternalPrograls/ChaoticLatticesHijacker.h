fa46c97dd38e422d10893ad2d183fa85e368406b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-11 12:35:58 +0200

    Create ChaoticLatticesHijacker.h

---
+++ HijackingInternalPrograls/ChaoticLatticesHijacker.h
@@ -0,0 +1,3 @@
+//if we follow the way of having two types of
+//compo'ents in a graphics card that hold specific oscillator,
+//. and if we have to insert datas chaotic lattices are the way  next steps !
