42c99cd6ab6efab46a08f77b45a65999fdf667f7
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-13 10:20:05 +0200

    Create RawDarasToDeviceOwnerSelecyor.h

---
+++ ShaderLibrary/RawDarasToDeviceOwnerSelecyor.h
@@ -0,0 +1,4 @@
+//distributed inductance here doenst need component
+//specifications. so we can use rk4 algorithm 
+//as its complexity satisfy zll our 'eeds here
+//(not heat transfer but czn be extended).
